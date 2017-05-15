ce5efe070583eb297773192d1e4ac2e24178e7c9
yiminghe <yiminghe@gmail.com>
2017-05-15 19:54:40 +0800

    Updates

--- examples/beforeUpload.js
+++ examples/beforeUpload.js
@@ -1,80 +1,81 @@
-webpackJsonp([0],[
-/* 0 */
-/***/ function(module, exports, __webpack_require__) {
+webpackJsonp([1],{
 
-	module.exports = __webpack_require__(1);
+/***/ 135:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__(38);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react_dom__ = __webpack_require__(70);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_react_dom__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rc_upload__ = __webpack_require__(88);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rc_upload___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_rc_upload__);
+/* eslint no-console:0 */
 
 
-/***/ },
-/* 1 */
-/***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	var _react = __webpack_require__(2);
-	
-	var _react2 = _interopRequireDefault(_react);
-	
-	var _reactDom = __webpack_require__(33);
-	
-	var _reactDom2 = _interopRequireDefault(_reactDom);
-	
-	var _rcUpload = __webpack_require__(179);
-	
-	var _rcUpload2 = _interopRequireDefault(_rcUpload);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	var props = {
-	  action: '/upload.do',
-	  multiple: true,
-	  onStart: function onStart(file) {
-	    console.log('onStart', file, file.name);
-	  },
-	  onSuccess: function onSuccess(ret) {
-	    console.log('onSuccess', ret);
-	  },
-	  onError: function onError(err) {
-	    console.log('onError', err);
-	  },
-	  beforeUpload: function beforeUpload(file, fileList) {
-	    console.log(file, fileList);
-	    return new Promise(function (resolve) {
-	      console.log('start check');
-	      setTimeout(function () {
-	        console.log('check finshed');
-	        resolve(file);
-	      }, 3000);
-	    });
-	  }
-	}; /* eslint no-console:0 */
-	
-	var Test = function Test() {
-	  return _react2.default.createElement(
-	    'div',
-	    {
-	      style: {
-	        margin: 100
-	      }
-	    },
-	    _react2.default.createElement(
-	      'div',
-	      null,
-	      _react2.default.createElement(
-	        _rcUpload2.default,
-	        props,
-	        _react2.default.createElement(
-	          'a',
-	          null,
-	          '\u5F00\u59CB\u4E0A\u4F20'
-	        )
-	      )
-	    )
-	  );
-	};
-	
-	_reactDom2.default.render(_react2.default.createElement(Test, null), document.getElementById('__react-content'));
 
-/***/ }
-]);
+
+var props = {
+  action: '/upload.do',
+  multiple: true,
+  onStart: function onStart(file) {
+    console.log('onStart', file, file.name);
+  },
+  onSuccess: function onSuccess(ret) {
+    console.log('onSuccess', ret);
+  },
+  onError: function onError(err) {
+    console.log('onError', err);
+  },
+  beforeUpload: function beforeUpload(file, fileList) {
+    console.log(file, fileList);
+    return new Promise(function (resolve) {
+      console.log('start check');
+      setTimeout(function () {
+        console.log('check finshed');
+        resolve(file);
+      }, 3000);
+    });
+  }
+};
+
+var Test = function Test() {
+  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+    'div',
+    {
+      style: {
+        margin: 100
+      }
+    },
+    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+      'div',
+      null,
+      __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+        __WEBPACK_IMPORTED_MODULE_2_rc_upload___default.a,
+        props,
+        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+          'a',
+          null,
+          '\u5F00\u59CB\u4E0A\u4F20'
+        )
+      )
+    )
+  );
+};
+
+__WEBPACK_IMPORTED_MODULE_1_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(Test, null), document.getElementById('__react-content'));
+
+/***/ }),
+
+/***/ 282:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(135);
+
+
+/***/ })
+
+},[282]);
 //# sourceMappingURL=beforeUpload.js.map
--- examples/simple.js
+++ examples/simple.js
@@ -1,175 +1,175 @@
-webpackJsonp([1],{
+webpackJsonp([0],{
 
-/***/ 0:
-/***/ function(module, exports, __webpack_require__) {
+/***/ 136:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-	module.exports = __webpack_require__(275);
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_extends__ = __webpack_require__(41);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_extends___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_extends__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_classCallCheck__ = __webpack_require__(39);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_classCallCheck___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_classCallCheck__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_createClass__ = __webpack_require__(40);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_createClass___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_createClass__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_babel_runtime_helpers_possibleConstructorReturn__ = __webpack_require__(43);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_babel_runtime_helpers_possibleConstructorReturn___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_babel_runtime_helpers_possibleConstructorReturn__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_babel_runtime_helpers_inherits__ = __webpack_require__(42);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_babel_runtime_helpers_inherits___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_babel_runtime_helpers_inherits__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_react__ = __webpack_require__(38);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_react_dom__ = __webpack_require__(70);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_6_react_dom__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7_rc_upload__ = __webpack_require__(88);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7_rc_upload___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_7_rc_upload__);
 
 
-/***/ },
 
-/***/ 275:
-/***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	var _extends2 = __webpack_require__(182);
-	
-	var _extends3 = _interopRequireDefault(_extends2);
-	
-	var _classCallCheck2 = __webpack_require__(220);
-	
-	var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
-	
-	var _possibleConstructorReturn2 = __webpack_require__(221);
-	
-	var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
-	
-	var _inherits2 = __webpack_require__(257);
-	
-	var _inherits3 = _interopRequireDefault(_inherits2);
-	
-	var _react = __webpack_require__(2);
-	
-	var _react2 = _interopRequireDefault(_react);
-	
-	var _reactDom = __webpack_require__(33);
-	
-	var _reactDom2 = _interopRequireDefault(_reactDom);
-	
-	var _rcUpload = __webpack_require__(179);
-	
-	var _rcUpload2 = _interopRequireDefault(_rcUpload);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	var style = '\n        .rc-upload-disabled {\n           opacity:0.5;\n        '; /* eslint no-console:0 */
-	
-	var Test = function (_React$Component) {
-	  (0, _inherits3.default)(Test, _React$Component);
-	
-	  function Test(props) {
-	    (0, _classCallCheck3.default)(this, Test);
-	
-	    var _this = (0, _possibleConstructorReturn3.default)(this, _React$Component.call(this, props));
-	
-	    _this.destroy = function () {
-	      _this.setState({
-	        destroyed: true
-	      });
-	    };
-	
-	    _this.uploaderProps = {
-	      action: '/upload.do',
-	      data: { a: 1, b: 2 },
-	      headers: {
-	        Authorization: 'xxxxxxx'
-	      },
-	      multiple: true,
-	      beforeUpload: function beforeUpload(file) {
-	        console.log('beforeUpload', file.name);
-	      },
-	
-	      onStart: function onStart(file) {
-	        console.log('onStart', file.name);
-	        // this.refs.inner.abort(file);
-	      },
-	      onSuccess: function onSuccess(file) {
-	        console.log('onSuccess', file);
-	      },
-	      onProgress: function onProgress(step, file) {
-	        console.log('onProgress', Math.round(step.percent), file.name);
-	      },
-	      onError: function onError(err) {
-	        console.log('onError', err);
-	      }
-	    };
-	    _this.state = {
-	      destroyed: false
-	    };
-	    return _this;
-	  }
-	
-	  Test.prototype.render = function render() {
-	    if (this.state.destroyed) {
-	      return null;
-	    }
-	    return _react2.default.createElement(
-	      'div',
-	      {
-	        style: {
-	          margin: 100
-	        }
-	      },
-	      _react2.default.createElement(
-	        'h2',
-	        null,
-	        '\u56FA\u5B9A\u4F4D\u7F6E'
-	      ),
-	      _react2.default.createElement(
-	        'style',
-	        null,
-	        style
-	      ),
-	      _react2.default.createElement(
-	        'div',
-	        null,
-	        _react2.default.createElement(
-	          _rcUpload2.default,
-	          (0, _extends3.default)({}, this.uploaderProps, { ref: 'inner' }),
-	          _react2.default.createElement(
-	            'a',
-	            null,
-	            '\u5F00\u59CB\u4E0A\u4F20'
-	          )
-	        )
-	      ),
-	      _react2.default.createElement(
-	        'h2',
-	        null,
-	        '\u6EDA\u52A8'
-	      ),
-	      _react2.default.createElement(
-	        'div',
-	        {
-	          style: {
-	            height: 200,
-	            overflow: 'auto',
-	            border: '1px solid red'
-	          }
-	        },
-	        _react2.default.createElement(
-	          'div',
-	          {
-	            style: {
-	              height: 500
-	            }
-	          },
-	          _react2.default.createElement(
-	            _rcUpload2.default,
-	            (0, _extends3.default)({}, this.uploaderProps, { component: 'div', style: { display: 'inline-block' } }),
-	            _react2.default.createElement(
-	              'a',
-	              null,
-	              '\u5F00\u59CB\u4E0A\u4F202'
-	            )
-	          )
-	        )
-	      ),
-	      _react2.default.createElement(
-	        'button',
-	        { onClick: this.destroy },
-	        'destroy'
-	      )
-	    );
-	  };
-	
-	  return Test;
-	}(_react2.default.Component);
-	
-	_reactDom2.default.render(_react2.default.createElement(Test, null), document.getElementById('__react-content'));
 
-/***/ }
+/* eslint no-console:0 */
 
-});
+
+
+
+
+var style = '\n        .rc-upload-disabled {\n           opacity:0.5;\n        ';
+
+var Test = function (_React$Component) {
+  __WEBPACK_IMPORTED_MODULE_4_babel_runtime_helpers_inherits___default()(Test, _React$Component);
+
+  function Test(props) {
+    __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_classCallCheck___default()(this, Test);
+
+    var _this = __WEBPACK_IMPORTED_MODULE_3_babel_runtime_helpers_possibleConstructorReturn___default()(this, (Test.__proto__ || Object.getPrototypeOf(Test)).call(this, props));
+
+    _this.destroy = function () {
+      _this.setState({
+        destroyed: true
+      });
+    };
+
+    _this.uploaderProps = {
+      action: '/upload.do',
+      data: { a: 1, b: 2 },
+      headers: {
+        Authorization: 'xxxxxxx'
+      },
+      multiple: true,
+      beforeUpload: function beforeUpload(file) {
+        console.log('beforeUpload', file.name);
+      },
+
+      onStart: function onStart(file) {
+        console.log('onStart', file.name);
+        // this.refs.inner.abort(file);
+      },
+      onSuccess: function onSuccess(file) {
+        console.log('onSuccess', file);
+      },
+      onProgress: function onProgress(step, file) {
+        console.log('onProgress', Math.round(step.percent), file.name);
+      },
+      onError: function onError(err) {
+        console.log('onError', err);
+      }
+    };
+    _this.state = {
+      destroyed: false
+    };
+    return _this;
+  }
+
+  __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_createClass___default()(Test, [{
+    key: 'render',
+    value: function render() {
+      if (this.state.destroyed) {
+        return null;
+      }
+      return __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(
+        'div',
+        {
+          style: {
+            margin: 100
+          }
+        },
+        __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(
+          'h2',
+          null,
+          '\u56FA\u5B9A\u4F4D\u7F6E'
+        ),
+        __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(
+          'style',
+          null,
+          style
+        ),
+        __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(
+          'div',
+          null,
+          __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(
+            __WEBPACK_IMPORTED_MODULE_7_rc_upload___default.a,
+            __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_extends___default()({}, this.uploaderProps, { ref: 'inner' }),
+            __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(
+              'a',
+              null,
+              '\u5F00\u59CB\u4E0A\u4F20'
+            )
+          )
+        ),
+        __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(
+          'h2',
+          null,
+          '\u6EDA\u52A8'
+        ),
+        __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(
+          'div',
+          {
+            style: {
+              height: 200,
+              overflow: 'auto',
+              border: '1px solid red'
+            }
+          },
+          __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(
+            'div',
+            {
+              style: {
+                height: 500
+              }
+            },
+            __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(
+              __WEBPACK_IMPORTED_MODULE_7_rc_upload___default.a,
+              __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_extends___default()({}, this.uploaderProps, { component: 'div', style: { display: 'inline-block' } }),
+              __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(
+                'a',
+                null,
+                '\u5F00\u59CB\u4E0A\u4F202'
+              )
+            )
+          )
+        ),
+        __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(
+          'button',
+          { onClick: this.destroy },
+          'destroy'
+        )
+      );
+    }
+  }]);
+
+  return Test;
+}(__WEBPACK_IMPORTED_MODULE_5_react___default.a.Component);
+
+__WEBPACK_IMPORTED_MODULE_6_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(Test, null), document.getElementById('__react-content'));
+
+/***/ }),
+
+/***/ 283:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(136);
+
+
+/***/ })
+
+},[283]);
 //# sourceMappingURL=simple.js.map
