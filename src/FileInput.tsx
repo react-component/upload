c407423e3d3bfae131de34dfef6dc4df8b9206dc
fung-csf <67588367+fung-csf@users.noreply.github.com>
2021-08-28 08:56:29 +0800

    Update FileInput.tsx
    change function name from inputFileValue to FileContent

--- src/FileInput.tsx
+++ src/FileInput.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function FileInput({ value, inputFileValue, ...rest }) {
+function FileInput({ value, FileValue, ...rest }) {
   const [input, setInput] = useState(false);
   const [Files, setFiles] = useState([]);
 
