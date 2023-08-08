0131f92ea134c3b07246908fea051d6d890a2179
zhe-he <zhe-he@qq.com>
2023-08-08 16:03:08 +0800

    feat: remove uid

--- src/AjaxUploader.tsx
+++ src/AjaxUploader.tsx
@@ -235,6 +235,8 @@ class AjaxUploader extends Component<UploadProps> {
     this.setState({
       uid: getUid(),
     });
+    // After changing here, can uid be removed?
+    if (this.fileInput) this.fileInput.value = "";
   }
 
   abort(file?: any) {
