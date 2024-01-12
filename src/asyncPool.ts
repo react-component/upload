6125810eed3d166f3420ee38e3b560c07d90ce7e
Ryan Wang <thepoy@163.com>
2024-01-12 23:51:57 +0800

    chore: add some semicolons

--- src/asyncPool.ts
+++ src/asyncPool.ts
@@ -3,11 +3,11 @@
  *
  * @template T - Type of the input items.
  * @template U - Type of the result of the asynchronous task.
- * 
+ *
  * @param {number} concurrencyLimit - The maximum number of asynchronous tasks to execute concurrently.
  * @param {T[]} items - The array of items to process asynchronously.
  * @param {(item: T) => Promise<U>} asyncTask - The asynchronous task to be performed on each item.
- * 
+ *
  * @returns {Promise<U[]>} - A promise that resolves to an array of results from the asynchronous tasks.
  */
 export default async function asyncPool<T, U>(
@@ -16,8 +16,7 @@ export default async function asyncPool<T, U>(
   asyncTask: (item: T) => Promise<U>,
 ): Promise<U[]> {
   const tasks: Promise<U>[] = [];
-  const pendings: Promise<U>[] = []
-
+  const pendings: Promise<U>[] = [];
 
   for (const item of items) {
     const task = asyncTask(item);
@@ -25,15 +24,15 @@ export default async function asyncPool<T, U>(
 
     if (concurrencyLimit <= items.length) {
       task.then(() => {
-        pendings.splice(pendings.indexOf(task), 1)
-      })
-      pendings.push(task)
+        pendings.splice(pendings.indexOf(task), 1);
+      });
+      pendings.push(task);
 
       if (pendings.length >= concurrencyLimit) {
-        await Promise.race(pendings)
+        await Promise.race(pendings);
       }
     }
   }
 
-  return Promise.all(tasks)
+  return Promise.all(tasks);
 }
