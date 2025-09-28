c0cf1a612f2905c09407a9221d7c09bda0701aee
刘欢 <lh01217311@antgroup.com>
2025-09-28 16:11:29 +0800

    feat: modify  rc-util path in component

--- src/attr-accept.ts
+++ src/attr-accept.ts
@@ -1,4 +1,4 @@
-import warning from '@rc-component/util/lib/warning';
+import { warning } from '@rc-component/util';
 import type { RcFile } from './interface';
 
 export default (file: RcFile, acceptedFiles: string | string[]) => {
