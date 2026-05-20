f5c3f9569cbbeccd141b8531725838f1dfc71ea5
高艳兵 <yanbing.gao@aliyun.com>
2026-05-20 11:41:47 +0800

    chore: address review comments

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import type {
   AcceptConfig,
+  BeforeUploadFileType,
+  CustomUploadRequestOption,
   RcFile,
+  UploadProgressEvent,
   UploadProps,
+  UploadRequestError,
   UploadRequestOption,
 } from './interface';
 import Upload from './Upload';
 
-export type { AcceptConfig, RcFile, UploadProps, UploadRequestOption };
+export type {
+  AcceptConfig,
+  BeforeUploadFileType,
+  CustomUploadRequestOption,
+  RcFile,
+  UploadProgressEvent,
+  UploadProps,
+  UploadRequestError,
+  UploadRequestOption,
+};
 
 export default Upload;
